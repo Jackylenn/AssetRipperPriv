a4259cc24aeb1a80da60c1555a9ac33056024ed7
Jackylenn <tiktokvrcontent@gmail.com>
2026-04-02 19:14:43 +0200

    DummyShaderTextExporter.cs
    im sorry if this is on main branch

--- Source/AssetRipper.Export.UnityProjects/Shaders/DummyShaderTextExporter.cs
+++ Source/AssetRipper.Export.UnityProjects/Shaders/DummyShaderTextExporter.cs
@@ -1,4 +1,5 @@
-﻿using AssetRipper.Assets;
+using AssetRipper.Assets;
+using AssetRipper.Assets.Generics;
 using AssetRipper.SourceGenerated.Classes.ClassID_48;
 using AssetRipper.SourceGenerated.Extensions;
 using AssetRipper.SourceGenerated.Extensions.Enums.Shader.SerializedShader;
@@ -9,31 +10,6 @@ namespace AssetRipper.Export.UnityProjects.Shaders;
 
 public sealed class DummyShaderTextExporter : ShaderExporterBase
 {
-	// This uses CGPROGRAM instead of HLSLPROGRAM because the latter was supposedly introduced in Unity 5.6.
-	// https://github.com/UnityCommunity/UnityReleaseNotes/blob/7b417b8ff64415e1e509d8c345b829c7cc11b650/5.6-Beta/5.6.0b1.txt#L143
-	private static string FallbackDummyShader { get; } = """
-
-			SubShader{
-				Tags { "RenderType" = "Opaque" }
-				LOD 200
-				CGPROGRAM
-		#pragma surface surf Lambert
-		#pragma target 3.0
-				sampler2D _MainTex;
-				struct Input
-				{
-					float2 uv_MainTex;
-				};
-				void surf(Input IN, inout SurfaceOutput o)
-				{
-					float4 c = tex2D(_MainTex, IN.uv_MainTex);
-					o.Albedo = c.rgb;
-				}
-				ENDCG
-			}
-
-		""".Replace("\r", "");
-
 	public override bool Export(IExportContainer container, IUnityObjectBase asset, string path, FileSystem fileSystem)
 	{
 		return ExportShader((IShader)asset, path, fileSystem);
@@ -48,23 +24,12 @@ public sealed class DummyShaderTextExporter : ShaderExporterBase
 
 	public static bool ExportShader(IShader shader, TextWriter writer)
 	{
-		// Technically, this outputs invalid shader code for Unity 5.5 because HLSLPROGRAM was not introduced until Unity 5.6.
 		if (shader.Has_ParsedForm())
 		{
 			writer.Write($"Shader \"{shader.ParsedForm.Name}\" {{\n");
 			Export(shader.ParsedForm.PropInfo, writer);
-
-			TemplateShader templateShader = TemplateList.GetBestTemplate(shader);
 			writer.Write("\t//DummyShaderTextExporter\n");
-			if (templateShader != null)
-			{
-				writer.Write(templateShader.ShaderText);
-			}
-			else
-			{
-				writer.WriteIndent(1);
-				writer.Write(FallbackDummyShader);
-			}
+			WriteGeneratedShaderBody(shader, writer);
 			writer.Write('\n');
 
 			if (shader.ParsedForm.FallbackName != string.Empty)
@@ -90,12 +55,150 @@ public sealed class DummyShaderTextExporter : ShaderExporterBase
 			writer.WriteString(header, 0, subshaderIndex);
 
 			writer.Write("\t//DummyShaderTextExporter\n");
-			writer.WriteIndent(1);
-			writer.Write(FallbackDummyShader);
+			writer.Write(GenericFallbackShader);
 
 			writer.Write('}');
 		}
 		return true;
+	}
+
+	private static void WriteGeneratedShaderBody(IShader shader, TextWriter writer)
+	{
+		(string? texturePropertyName, string? colorPropertyName) = GetPrimaryProperties(shader);
+		bool isTransparent = IsTransparentShader(shader);
+
+		if (texturePropertyName is null && colorPropertyName is null)
+		{
+			writer.Write(GenericFallbackShader);
+			return;
+		}
+
+		writer.Write("\tSubShader{\n");
+		if (isTransparent)
+		{
+			writer.Write("\t\tTags { \"RenderType\"=\"Transparent\" \"Queue\"=\"Transparent\" }\n");
+			writer.Write("\t\tLOD 200\n");
+			writer.Write("\t\tBlend SrcAlpha OneMinusSrcAlpha\n");
+			writer.Write("\t\tZWrite Off\n\n");
+		}
+		else
+		{
+			writer.Write("\t\tTags { \"RenderType\"=\"Opaque\" }\n");
+			writer.Write("\t\tLOD 200\n\n");
+		}
+
+		writer.Write("\t\tPass\n");
+		writer.Write("\t\t{\n");
+		writer.Write("\t\t\tHLSLPROGRAM\n");
+		writer.Write("\t\t\t#pragma vertex vert\n");
+		writer.Write("\t\t\t#pragma fragment frag\n\n");
+		writer.Write("\t\t\tfloat4x4 unity_ObjectToWorld;\n");
+		writer.Write("\t\t\tfloat4x4 unity_MatrixVP;\n");
+		if (texturePropertyName is not null)
+		{
+			writer.Write($"\t\t\tfloat4 {texturePropertyName}_ST;\n");
+		}
+		if (colorPropertyName is not null)
+		{
+			writer.Write($"\t\t\tfloat4 {colorPropertyName};\n");
+		}
+		writer.Write('\n');
+
+		writer.Write("\t\t\tstruct Vertex_Stage_Input\n");
+		writer.Write("\t\t\t{\n");
+		writer.Write("\t\t\t\tfloat4 pos : POSITION;\n");
+		if (texturePropertyName is not null)
+		{
+			writer.Write("\t\t\t\tfloat2 uv : TEXCOORD0;\n");
+		}
+		writer.Write("\t\t\t};\n\n");
+
+		writer.Write("\t\t\tstruct Vertex_Stage_Output\n");
+		writer.Write("\t\t\t{\n");
+		if (texturePropertyName is not null)
+		{
+			writer.Write("\t\t\t\tfloat2 uv : TEXCOORD0;\n");
+		}
+		writer.Write("\t\t\t\tfloat4 pos : SV_POSITION;\n");
+		writer.Write("\t\t\t};\n\n");
+
+		writer.Write("\t\t\tVertex_Stage_Output vert(Vertex_Stage_Input input)\n");
+		writer.Write("\t\t\t{\n");
+		writer.Write("\t\t\t\tVertex_Stage_Output output;\n");
+		if (texturePropertyName is not null)
+		{
+			writer.Write($"\t\t\t\toutput.uv = (input.uv.xy * {texturePropertyName}_ST.xy) + {texturePropertyName}_ST.zw;\n");
+		}
+		writer.Write("\t\t\t\toutput.pos = mul(unity_MatrixVP, mul(unity_ObjectToWorld, input.pos));\n");
+		writer.Write("\t\t\t\treturn output;\n");
+		writer.Write("\t\t\t}\n\n");
+
+		if (texturePropertyName is not null)
+		{
+			writer.Write($"\t\t\tTexture2D<float4> {texturePropertyName};\n");
+			writer.Write($"\t\t\tSamplerState sampler{texturePropertyName};\n\n");
+		}
+
+		writer.Write("\t\t\tfloat4 frag(Vertex_Stage_Output input) : SV_TARGET\n");
+		writer.Write("\t\t\t{\n");
+		string colorExpression = GetColorExpression(texturePropertyName, colorPropertyName);
+		writer.Write($"\t\t\t\treturn {colorExpression};\n");
+		writer.Write("\t\t\t}\n\n");
+
+		writer.Write("\t\t\tENDHLSL\n");
+		writer.Write("\t\t}\n");
+		writer.Write("\t}\n");
+	}
+
+	private static string GetColorExpression(string? texturePropertyName, string? colorPropertyName)
+	{
+		string textureExpression = texturePropertyName is null
+			? "float4(1.0, 1.0, 1.0, 1.0)"
+			: $"{texturePropertyName}.Sample(sampler{texturePropertyName}, input.uv.xy)";
+		return colorPropertyName is null
+			? textureExpression
+			: $"{textureExpression} * {colorPropertyName}";
+	}
+
+	private static (string? TexturePropertyName, string? ColorPropertyName) GetPrimaryProperties(IShader shader)
+	{
+		AccessListBase<ISerializedProperty>? properties = shader.ParsedForm?.PropInfo.Props;
+		if (properties is null || properties.Count == 0)
+		{
+			return default;
+		}
+
+		string? textureProperty = GetPreferredPropertyName(
+			properties,
+			SerializedPropertyType.Texture,
+			new[] { "_MainTex", "_BaseMap", "_BaseColorMap", "_BaseColorTexture", "_Albedo", "_Diffuse", "_DiffuseMap", "_MainTexture" });
+		string? colorProperty = GetPreferredPropertyName(
+			properties,
+			SerializedPropertyType.Color,
+			new[] { "_Color", "_BaseColor", "_BaseColour", "_TintColor", "_MainColor" });
+		return (textureProperty, colorProperty);
+	}
+
+	private static string? GetPreferredPropertyName(AccessListBase<ISerializedProperty> properties, SerializedPropertyType propertyType, IReadOnlyList<string> preferredNames)
+	{
+		foreach (string preferredName in preferredNames)
+		{
+			ISerializedProperty? match = properties.FirstOrDefault(property => property.GetType_() == propertyType && property.Name == preferredName);
+			if (match is not null)
+			{
+				return match.Name;
+			}
+		}
+
+		return properties.FirstOrDefault(property => property.GetType_() == propertyType)?.Name;
+	}
+
+	private static bool IsTransparentShader(IShader shader)
+	{
+		string shaderName = shader.ParsedForm?.Name ?? string.Empty;
+		return shaderName.Contains("transparent", StringComparison.OrdinalIgnoreCase)
+			|| shaderName.Contains("fade", StringComparison.OrdinalIgnoreCase)
+			|| shaderName.Contains("alpha", StringComparison.OrdinalIgnoreCase);
 	}
 
 	private static void Export(ISerializedProperties _this, TextWriter writer)
@@ -218,4 +321,45 @@ public sealed class DummyShaderTextExporter : ShaderExporterBase
 		}
 		writer.Write('\n');
 	}
+
+	private const string GenericFallbackShader = """
+	SubShader{
+		Tags { "RenderType" = "Opaque" }
+		LOD 200
+
+		Pass
+		{
+			HLSLPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			float4x4 unity_ObjectToWorld;
+			float4x4 unity_MatrixVP;
+
+			struct Vertex_Stage_Input
+			{
+				float4 pos : POSITION;
+			};
+
+			struct Vertex_Stage_Output
+			{
+				float4 pos : SV_POSITION;
+			};
+
+			Vertex_Stage_Output vert(Vertex_Stage_Input input)
+			{
+				Vertex_Stage_Output output;
+				output.pos = mul(unity_MatrixVP, mul(unity_ObjectToWorld, input.pos));
+				return output;
+			}
+
+			float4 frag(Vertex_Stage_Output input) : SV_TARGET
+			{
+				return float4(1.0, 1.0, 1.0, 1.0);
+			}
+
+			ENDHLSL
+		}
+	}
+	""".Replace("\r", "");
 }
